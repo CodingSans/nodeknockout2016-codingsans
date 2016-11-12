ca988d2ed41a5d27bb8c5aa30248674918a8de82
Laszlo Csele <laszlo.csele90@gmail.com>
2016-11-12 23:44:10 +0100

    google maps init

--- app/client/src/components/map/map.component.ts
+++ app/client/src/components/map/map.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ds-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent { }
+export class MapComponent implements OnInit {
+  @ViewChild('mapElement') mapElement: ElementRef;
+
+  ngOnInit() {
+    var hungary = {lat: 47.497832, lng: 19.045497};
+    var map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 4,
+      center: hungary
+    });
+    var marker = new google.maps.Marker({
+      position: hungary,
+      map: map
+    });
+  }
+}
