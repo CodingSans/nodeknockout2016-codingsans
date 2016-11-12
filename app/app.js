86fbb5da2a60b5a6fbaec29efa68dc9bd2e0e229
LaTotty <latotty91@gmail.com>
2016-11-12 10:57:35 +0100

    initial structure

--- app/app.js
+++ app/app.js
@@ -2,10 +2,9 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello from Node Knockout 2016!')
+  response.send('Hello from Node Knockout 2016 CodingSans!')
 })
 
 app.listen(app.get('port'), function() {
